a12f1961af7d5a741dd1ad5220ac909226495eb1
Mahakrthik01 <mahakarthikbro@gmail.com>
2024-10-18 16:34:26 +0930

    main game loop *important*

--- Python-Game/_.MainLoop-Directory._.py
+++ Python-Game/_.MainLoop-Directory._.py
@@ -1,1 +1,3 @@
 # The Main Game Loop can be found in (.venv) -> Main Game Loop.
+# Im not sure why my github is not detecting the main game codes. 
+# Please download the zipped file with the full codes.
